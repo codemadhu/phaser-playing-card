0de612679eb8bfcc44f4b4ed6f2acda737436ea3
Madhukar Thapa Magar <idomadhukar@gmail.com>
2023-09-28 22:42:35 +0545

    Added Card class

--- src/objects/Card.ts
+++ src/objects/Card.ts
@@ -1,44 +1,110 @@
-import Phaser = require("phaser");
+import "phaser";
 
 export interface CardConfig {
   scene: Phaser.Scene;
-  position: Position;
-  texture: string;
   suit: string;
   rank: number;
-  isFaceUp: boolean;
+  texture: string;
+  isFaceUp?: boolean;
+  options?: {
+    flipSpeed?: number;
+    flipZoom?: number;
+  };
 }
 
-type Position = {
-  x: number;
-  y: number;
-};
+export class Card extends Phaser.GameObjects.Container {
+  private isFaceUp: boolean;
+  private isFlipping: boolean;
+  private flipAnimationTimeline: Phaser.Time.Timeline;
 
-export class Card extends Phaser.GameObjects.Sprite {
   constructor(private config: CardConfig) {
-    super(
-      config.scene,
-      config.position.x,
-      config.position.y,
-      config.texture,
-      "back-red"
+    super(config.scene, 0, 0);
+
+    this.isFaceUp = this.config.isFaceUp || false;
+    this.isFlipping = false;
+
+    this.createCard();
+    this.flipAnimationTimeline = this.createFlipAnimationTimeline();
+  }
+
+  public flip() {
+    if (!this.isFlipping) this.flipAnimationTimeline.play();
+  }
+
+  private createFlipAnimationTimeline(): Phaser.Time.Timeline {
+    const totalTween = 4;
+    const flipSpeed = this.config.options?.flipSpeed || 500;
+    const duration = flipSpeed / totalTween;
+    const sprite = this.getAt(0) as Phaser.GameObjects.Sprite;
+
+    return this.scene.add.timeline([
+      {
+        at: 0,
+        tween: {
+          targets: sprite,
+          scale: 1.2,
+          duration,
+          onStart: () => {
+            this.isFlipping = true;
+          },
+        },
+      },
+      {
+        at: duration,
+        tween: {
+          targets: sprite,
+          scaleX: 0,
+          duration,
+          onComplete: () => {
+            const name = this.isFaceUp
+              ? "back-red"
+              : `${this.config.suit}${this.config.rank}`;
+            sprite.setTexture(this.config.texture, name);
+          },
+        },
+      },
+      {
+        at: duration * 2,
+        tween: {
+          targets: sprite,
+          scale: 1.1,
+          duration,
+        },
+      },
+      {
+        at: duration * 3,
+        tween: {
+          targets: sprite,
+          scale: 1,
+          duration: duration / 4,
+          onComplete: () => {
+            this.isFlipping = false;
+            this.isFaceUp = !this.isFaceUp;
+          },
+        },
+      },
+    ]);
+  }
+
+  private createCard() {
+    const name = this.isFaceUp
+      ? `${this.config.suit}${this.config.rank}`
+      : "back-red";
+
+    const card = new Phaser.GameObjects.Sprite(
+      this.scene,
+      0,
+      0,
+      this.config.texture,
+      name
     );
-    this.config = config;
-  }
 
-  public getInfo(): {
-    rank: number;
-    suit: string;
-    isFaceUp: boolean;
-    position: Position;
-  } {
-    const { rank, suit, isFaceUp, position } = this.config;
-    return { rank, suit, isFaceUp, position };
-  }
+    this.add(card);
 
-  //   public flipCard() {
-  //     !this.faceUp
-  //       ? this.setTexture(this.atlas, this.cardName)
-  //       : this.setTexture(this.atlas, "back-red");
-  //   }
+    const { width, height } = card;
+    this.setInteractive(
+      new Phaser.Geom.Rectangle(-width / 2, -height / 2, width, height),
+      Phaser.Geom.Rectangle.Contains
+    );
+  }
 }
